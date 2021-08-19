e5e733b9de3042e4aa5f7a69e9f4c8ae4e0257d9
jaycedowell <jayce.dowell@gmail.com>
2021-08-18 19:18:51 -0600

    Added the concept of 'slots' to the beamformer control to allow commands to be executed at certains times.

--- pipeline-control/lwa352_pipeline_control/blocks/beamform_control.py
+++ pipeline-control/lwa352_pipeline_control/blocks/beamform_control.py
@@ -4,7 +4,15 @@ import numpy as np
 from .block_control_base import BlockControl
 
 class BeamformControl(BlockControl):
-    def update_calibration_gains(self, input_id, gains):
+    @staticmethod
+    def _get_when_to_execute(slot):
+        """
+        Static method to convert a slot value into a unix-style timestamp.
+        
+        """
+        return int(time.time()) + 1 + slot/100.0
+
+    def update_calibration_gains(self, input_id, gains, slot=0):
         """
         Update calibration gains for a single beam and input.
 
@@ -33,10 +41,11 @@ class BeamformControl(BlockControl):
                 'type': 'calibration',
                 'input_id': input_id,
                 'data': gains_real.tolist(),
+                'when': self._get_when_to_execute(slot)
             }
         )
 
-    def update_gains(self, beam_id, gains):
+    def update_gains(self, beam_id, gains, slot=0):
         """
         Update the dipole gains for a single beam.
         
@@ -55,10 +64,11 @@ class BeamformControl(BlockControl):
                 'type': 'gains',
                 'beam_id': beam_id,
                 'data': gains.tolist(),
+                'when': self._get_when_to_execute(slot)
             }
         )
 
-    def update_delays(self, beam_id, delays):
+    def update_delays(self, beam_id, delays, slot=0):
         """
         Update geometric delays for a single beam.
 
@@ -77,5 +87,6 @@ class BeamformControl(BlockControl):
                 'type': 'delays',
                 'beam_id': beam_id,
                 'data': delays.tolist(),
+                'when': self._get_when_to_execute(slot)
             }
         )
